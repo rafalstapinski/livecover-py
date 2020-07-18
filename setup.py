428c7e088032272b82acfa73c03ec2c2ec0bfe8d
Rafal Stapinski <stapinskirafal@gmail.com>
2020-07-17 21:23:01 -0400

    0.0.2

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
     name="livecover",
     packages=["livecover"],
-    version="0.0.1",
+    version="0.0.2",
     license="BSD",
     description="Python library to report live code usage",
     author="Rafal Stapinski",
     author_email="stapinskirafal@gmail.com",
     url="https://github.com/rafalstapinski/livecover-py",
-    download_url="https://github.com/rafalstapinski/livecover-py/archive/0.0.1.tar.gz",
+    download_url="https://github.com/rafalstapinski/livecover-py/archive/0.0.2.tar.gz",
     keywords=["live", "code", "coverage",],
     install_requires=["coverage==5.2"],
     classifiers=[
